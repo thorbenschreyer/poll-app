bfb9fd75b8398f61b02c2f896c7a45dc78de91a1
thorbenschreyer <thorben.schreyer@web.de>
2026-08-21 14:14:02 +0200

    implements Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
+import { SurveyView } from './surveys/survey-view/survey-view';
+import { CreateSurvey } from './surveys/create-survey/create-survey';
+import { ErrorPage } from './error-page/error-page';
 
 export const routes: Routes = [
-    { path: "", component:MainPage}
+    { path: "", component:MainPage},
+    { path: "view-survey", component:SurveyView},
+    { path: "new-survey", component:CreateSurvey},
 
-
+    { path: "**", component:ErrorPage}
 ];
