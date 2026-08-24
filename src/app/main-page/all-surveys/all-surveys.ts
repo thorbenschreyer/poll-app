cb583f86c6a51552da35ea36e449570184a26a52
thorbenschreyer <thorben.schreyer@web.de>
2026-08-24 23:09:56 +0200

    add filterbuttons with function

--- src/app/main-page/all-surveys/all-surveys.ts
+++ src/app/main-page/all-surveys/all-surveys.ts
@@ -7,12 +7,21 @@ import { Component, signal } from '@angular/core';
   styleUrl: './all-surveys.scss',
 })
 export class AllSurveys {
-  isActive = signal(false);
+  categoryIsActive = signal(false);
   usedCategory = signal('');
+  pastSurvey = signal(false);
+  activeSurvey = signal(false);
+
+  filterActivePastSurvey() {
+    console.log("Past: " + this.pastSurvey)
+    console.log("Active" + this.activeSurvey)
+  }
+
+
 
   selectCategory(category: string) {
     this.usedCategory.set(category);
-    this.isActive.set(!this.isActive())
+    this.categoryIsActive.set(!this.categoryIsActive())
     if (this.usedCategory() == "All Surveys") {
       this.usedCategory.set("");
     }
