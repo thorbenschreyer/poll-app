a0c602bfea17d2acfd40eca9cd8d818b387600e3
thorbenschreyer <thorben.schreyer@web.de>
2026-08-25 19:18:21 +0200

    fix category picker, cleanUp

--- src/app/main-page/all-surveys/all-surveys.ts
+++ src/app/main-page/all-surveys/all-surveys.ts
@@ -11,46 +11,11 @@ import { Survey } from '../../interfaces/survey';
   styleUrl: './all-surveys.scss',
 })
 export class AllSurveys {
+  filterservice = inject(FilterService);
   categoryIsActive = signal(false);
   usedCategory = signal('All Surveys');
-  day = 1;
-
-  filterservice = inject(FilterService);
   sortedSurveylist = signal<Survey[]>(this.filterservice.surveyList());
-
-
-  
-  filterSurveyList(pastSurvey: boolean, activeSurvey: boolean, category: string) {
-    this.usedCategory.set(category);
-    const newList:Survey[] = this.filterservice.filterByActivity(pastSurvey, activeSurvey)
-    this.sortedSurveylist.set(this.filterservice.filterByCategory(this.usedCategory(), newList));
-  }
-
-
-  filterActivePastSurvey() {
-    console.log(this.sortedSurveylist());
-    console.log('Active' + this.filterservice.activeSurvey());
-  }
-
-  selectCategory(category: string) {
-    this.usedCategory.set(category);
-    this.categoryIsActive.set(!this.categoryIsActive());
-    this.filterSurveyList(this.filterservice.pastSurvey(), this.filterservice.activeSurvey(), category)
-
-    if (this.usedCategory() == 'All Surveys') {
-      this.sortedSurveylist.set(this.filterservice.surveyList());
-      this.usedCategory.set('');
-    }
-  }
-
-  setActivityFilter() {
-    this.sortedSurveylist.set(
-      this.filterservice.filterByActivity(
-        this.filterservice.pastSurvey(),
-        this.filterservice.activeSurvey(),
-      ),
-    );
-  }
+  day = 1;
 
   categorylist = [
     'All Surveys',
@@ -61,6 +26,37 @@ export class AllSurveys {
     'Lifestyle & Preferences',
     'Technology & Innovation',
   ];
-}
 
-// Hier kommt rein activeSurvey true dann mache die sortierung ansonsten die oder alles
+  /**
+   * Als erstes setzt diese funktion die verwendete Category.
+   * Dnn erstellen wir mit NewList ein gefiltertes Array das nach Actic und Nicht Active schaut
+   * Im letzten Schritt, filtern wir die sortedSurveyList nach der usedCategory
+   * @param pastSurvey
+   * @param activeSurvey
+   * @param category
+   */
+  filterSurveyList(pastSurvey: boolean, activeSurvey: boolean, category: string) {
+    this.usedCategory.set(category);
+    const newList: Survey[] = this.filterservice.filterByActivity(pastSurvey, activeSurvey);
+    this.sortedSurveylist.set(this.filterservice.filterByCategory(this.usedCategory(), newList));
+  }
+
+  /**
+   * Selcted die Kategorie und filtert diese dann. Die Funktion wird in filterSurveylist dargestellt
+   * @param category
+   */
+  selectCategory(category: string) {
+    this.usedCategory.set(category);
+    this.categoryIsActive.set(!this.categoryIsActive());
+    this.filterSurveyList(
+      this.filterservice.pastSurvey(),
+      this.filterservice.activeSurvey(),
+      category,
+    );
+
+    if (this.usedCategory() == 'All Surveys') {
+      this.sortedSurveylist.set(this.filterservice.surveyList());
+      this.usedCategory.set('');
+    }
+  }
+}
