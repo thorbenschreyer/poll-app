74b8e34c9b9216ab73e4d9712108a30579a13e0a
thorbenschreyer <thorben.schreyer@web.de>
2026-09-16 21:08:20 +0200

    implement checkbox and function

--- src/app/surveys/survey-view/survey-view.ts
+++ src/app/surveys/survey-view/survey-view.ts
@@ -13,17 +13,28 @@ import { DatePipe } from '@angular/common';
 export class SurveyView {
   private route = inject(ActivatedRoute);
   router = inject(Router);
-  filterService = inject(FilterService)
-  survey = this.filterService.SurveyDetail
+  filterService = inject(FilterService);
+  survey = this.filterService.SurveyDetail;
   createSurveyService = inject(CreateSurveyService);
-  noAnswers = false
+  noAnswers = false;
 
   ngOnInit() {
     let currentSurvey = this.route.snapshot.paramMap.get('id');
     console.log(currentSurvey);
-      if (currentSurvey) {
-    this.filterService.setSurveyDetailByID(currentSurvey);
-  }
+    if (currentSurvey) {
+      this.filterService.setSurveyDetailByID(currentSurvey);
+    }
   }
 
+  selectAnswer(questionId: string | undefined, answerId: string | undefined, event: Event) {
+    if (!questionId || !answerId) {
+      return;
+    }
+
+    const checkbox = event.target as HTMLInputElement;
+
+    console.log('Question:', questionId);
+    console.log('Answer:', answerId);
+    console.log('Checked:', checkbox.checked);
+  }
 }
